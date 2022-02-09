e689d1745aeb4f71ee1508f538ef345a8e904d69
ivk <ivk@tutao.de>
2022-02-09 17:21:54 +0100

    Improve GUI for native webauthn view

--- src/login/NativeWebauthnView.ts
+++ src/login/NativeWebauthnView.ts
@@ -3,35 +3,17 @@ import type {Children, Vnode} from "mithril"
 import m from "mithril"
 import type {WebauthnNativeBridge} from "../native/main/WebauthnNativeBridge"
 import {IWebauthn} from "../misc/2fa/webauthn/IWebauthn.js"
-
-const enum State {
-	None,
-	Registering,
-	Auth
-}
+import {SecondFactorImage} from "../gui/base/icons/Icons.js"
+import {progressIcon} from "../gui/base/Icon.js"
+import {lang} from "../misc/LanguageViewModel.js"
 
 export class NativeWebauthnView implements CurrentView {
-	private state: State = State.None
-
 	constructor(
 		private readonly webauthn: IWebauthn,
 		private readonly nativeTransport: WebauthnNativeBridge
 	) {
 		this.view = this.view.bind(this)
-		// TODO: maybe wrap in some gui idk
 		this.nativeTransport.init(this.webauthn)
-		// this.nativeTransport.init({
-		// 	onRegister: (challenge, id, name, displayName) => {
-		// 		this.state = State.Registering
-		// 		m.redraw()
-		// 		return this.doRegister(challenge, id, name, displayName)
-		// 	},
-		// 	onAuthenticate: (challenge, keys) => {
-		// 		this.state = State.Auth
-		// 		m.redraw()
-		// 		return this.doSign(challenge, keys)
-		// 	}
-		// })
 	}
 
 	updateUrl(args: Record<string, any>, requestedPath: string): void {
@@ -44,20 +26,13 @@ export class NativeWebauthnView implements CurrentView {
 					margin: "0 auto",
 				}
 			}, [
-				this.renderState(),
-				m("div", `webauthn support: ${String(this.webauthn.isSupported())}`),
+				m(".flex.col.justify-center.mt-xl", [
+					m(".flex-center", m("img[src=" + SecondFactorImage + "]")),
+					m(".mt.flex.col", [
+						m(".flex.justify-center", [m(".mr-s", progressIcon()), m("", lang.get("waitingForU2f_msg"))])
+					])
+				])
 			]
 		)
-	}
-
-	private renderState(): Children {
-		switch (this.state) {
-			case State.None:
-				return "-"
-			case State.Auth:
-				return "authenticating"
-			case State.Registering:
-				return "registering"
-		}
 	}
 }
